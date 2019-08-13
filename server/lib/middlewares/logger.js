c17e70ebda1fa4303b98ac89bfba2c93bebcf3b2
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-08-13 11:23:20 +0530

    bug fix log

--- server/lib/middlewares/logger.js
+++ server/lib/middlewares/logger.js
@@ -8,16 +8,67 @@
      :response-time ms */
 
 const random = function() {
-    return Math.random().toString(36).substr(2); // remove `0.`
+        return Math.floor(Math.pow(10, 9) + Math.random() * (Math.pow(10, 10) - Math.pow(10, 9) - 1));
 };
 
-const token = function() {
-    let token = random() + random();
-    return token.substr(1,19).replace(/\D/g, '')
-};
+const getCurrentTime = () => {
+    return `\x1b[36m${new Date().toUTCString()}\x1b[0m`;
+}
+
+const getToken = () => {
+    return `[\x1b[33m${random()}\x1b[0m]`
+}
+
+const getHTTPMethod = request => {
+    return request.method === 'DELETE' ? `\x1b[31m${request.method}\x1b[0m` : `\x1b[32m${request.method}\x1b[0m`
+}
+
+const getURL = request => {
+    return request.originalUrl
+}
+
+
+const getStatusCode = response => {
+    if(response.statusCode >= 500) {
+        return `\x1b[31m${response.statusCode}\x1b[0m`;
+    }
+
+    if (response.statusCode >= 400) {
+        console.log('Warn');
+        return `\x1b[31m${response.statusCode}\x1b[0m`;
+    }
+
+    return `\x1b[32m${response.statusCode}\x1b[0m`;
+}
+
+const getIP = request => {
+    return request.headers['x-forwarded-for'] || request.connection.remoteAddress || request.socket.remoteAddress ||(request.connection.socket ? request.connection.socket.remoteAddress : null)
+}
+
+const getUserAgent = request => {
+    return request.headers['user-agent']
+}
+
+getStatusMessage = response => {
+    return response.statusMessage;
+}
+
+getContentLength = response => {
+    return response.get('Content-Length') || 0 
+}
 
 exports.logger = (request, response, next) => {
-    //console.log('request', request.httpVersion);
-    console.log(`\x1b[36m${new Date().toUTCString()}\x1b[0m [\x1b[33m${token()}\x1b[0m] \x1b[32m${request.method}\x1b[0m ${request.url} ${request.httpVersion} ${response.statusCode} ${request.headers['x-forwarded-for'] || request.connection.remoteAddress || request.socket.remoteAddress ||(request.connection.socket ? request.connection.socket.remoteAddress : null), request.headers['user-agent']}`)
-    next()
+    response.on('finish', () => {
+        console.log(`${getCurrentTime()} ${getToken()} ${getHTTPMethod(request)} ${getURL(request)} ${getStatusCode(response)} ${getStatusMessage(response)} ${getContentLength(response)}b sent  ${getIP(request)} ${getUserAgent(request)}`)
+    })
+
+    response.on('close', () => {
+        console.log(`${getCurrentTime()} ${getToken()} ${getHTTPMethod(request)} ${getURL(request)} ${getStatusCode(response)} ${getStatusMessage(response)} ${getContentLength(response)}b sent  ${getIP(request)} ${getUserAgent(request)}`)
+    })
+
+    response.on('error', () => {
+        console.log(`${getCurrentTime()} ${getToken()} ${getHTTPMethod(request)} ${getURL(request)} ${getStatusCode(response)} ${getStatusMessage(response)} ${getContentLength(response)}b sent  ${getIP(request)} ${getUserAgent(request)}`)
+    })
+
+     next()
 }
