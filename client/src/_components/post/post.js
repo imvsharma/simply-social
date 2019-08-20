2f52ac0fb4868c5270e8a1f7c72638adc2f08f76
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-08-20 23:48:51 +0530

    user profile

--- client/src/_components/post/post.js
+++ client/src/_components/post/post.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import './post.scss';
+import UserProfilePic from '../user-profile-pic/userprofilepic';
 
 export default class Post extends React.Component {
     render () {
         return (
             <div className="post-container">
-                
+                <div className="post-header">
+                    <UserProfilePic />
+                </div>
             </div>
         )
     }
---
+++ client/src/_components/user-profile-pic/userprofilepic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './userprofilepic.scss';
+
+export default class UserProfilePic extends React.Component {
+    render () {
+        return(
+            <div className="userprofilecontainer"></div>
+        )
+    }
+}
