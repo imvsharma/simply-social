77e8acf6bc330dd799054800f6c734236f46993a
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-27 16:27:52 +0530

    schema creation for user create

--- server/graphql/schema/index.js
+++ server/graphql/schema/index.js
@@ -4,12 +4,21 @@ const userType = fileSystem.readFileSync(`${__dirname}/type/user.graphql`, 'utf8
 const AuthDataType = fileSystem.readFileSync(`${__dirname}/type/authdata.graphql`, 'utf8');
 const RootQuery = fileSystem.readFileSync(`${__dirname}/query/root.graphql`, 'utf8');
 
+//  Input
+const UserInput = fileSystem.readFileSync(`${__dirname}/input/user.graphql`, 'utf8');
+
+//  Mutation
+const RootMutation = fileSystem.readFileSync(`${__dirname}/mutation/root.graphql`, 'utf8');
+
 
 module.exports = buildSchema(`
     ${userType}
     ${AuthDataType}
     ${RootQuery}
+    ${UserInput}
+    ${RootMutation}
     schema {
         query: RootQuery
+        mutation: RootMutation
     }
 `)
