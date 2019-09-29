139b6f4943ac639828b6933100ba244660fe2848
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-29 23:06:09 +0530

    get user

--- server/graphql/resolvers/auth.js
+++ server/graphql/resolvers/auth.js
@@ -1,74 +1,102 @@
 
 
-const {userModel} = require('../../src/api/user/user.dao');
-const {decrypt} = require('../../helpers/decrypt');
-const {encrypt} = require('../../helpers/encrypt');
-const {generateToken} = require('../../helpers/generateToken');
+const { userModel } = require('../../src/api/user/user.dao');
+const { decrypt } = require('../../helpers/decrypt');
+const { encrypt } = require('../../helpers/encrypt');
+const { generateToken } = require('../../helpers/generateToken');
 
 module.exports = {
-    login: async ({email, password}) => {
-        const user = await userModel.findOne({email});
+    getUser: async ({ id, email }) => {
+        const user = await userModel.findOne(
+            {
+                $or: [
+                        { 
+                            email
+                        },
+                        { 
+                            _id: id 
+                        }
+                    ] 
+            }
+        );
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        return {
+            _id: user._id,
+            email: user.email
+        }
+    },
+
+    login: async ({ email, password }) => {
+        const user = await userModel.findOne({ email });
         console.log(user);
-        if(!user) {
-            throw new Error('User not found');
+        if (!user) {
+            throw new Error("User not found");
         }
 
         const isEqual = await decrypt(password, user.password);
-        if(!isEqual) {
-            throw new Error('Password mismatch');
+        if (!isEqual) {
+            throw new Error("Password mismatch");
         }
 
-        const token = await generateToken(user._id)
+        const token = await generateToken(user._id);
 
         return {
             userId: user.id,
             token: token,
             tokenExpiration: 1
-        }
+        };
     },
 
-    createUser: async (args) => {
-        const user = await userModel.findOne({email: args.user.email});
-        if(user) {
-            throw new Error('User already Exists');
+    createUser: async args => {
+        const user = await userModel.findOne({ email: args.user.email });
+        if (user) {
+            throw new Error("User already Exists");
         } else {
             const hashedPassword = await encrypt(args.user.password);
-            const {firstname, lastname, email} = args.user
+            const { firstname, lastname, email } = args.user;
             const user = new userModel({
                 firstname,
                 lastname,
                 email,
                 password: hashedPassword
-            })
+            });
 
             const savedUser = await user.save();
             return {
                 _id: savedUser._id
-            }
+            };
         }
     },
 
     updatePassword: async args => {
-        const user = await userModel.findOne({_id: args.id});
-        if(!user) {
+        const user = await userModel.findOne({ _id: args.id });
+        if (!user) {
             return {
                 success: false,
                 message: "User not found",
                 statusCode: 404
-            }
+            };
         } else {
             const hashedPassword = await encrypt(args.password);
-            const updatedUser = await userModel.findOneAndUpdate({_id: args.id}, {$set: {password: hashedPassword}}, {new: true});
-            if(!updatedUser) {
+            const updatedUser = await userModel.findOneAndUpdate(
+                { _id: args.id },
+                { $set: { password: hashedPassword } },
+                { new: true }
+            );
+            if (!updatedUser) {
                 return {
                     message: "user not updated"
-                }
+                };
             } else {
                 return {
                     success: true,
                     message: "password updated successfully"
-                }
+                };
             }
         }
     }
-}
+};
