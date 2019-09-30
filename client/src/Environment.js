c2c27340ccfd7b5bdfd3892e266fa3cb6fa4ba89
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-30 11:23:53 +0530

    client/ creating environment handle file for graphql

---
+++ client/src/Environment.js
@@ -0,0 +1,28 @@
+const {Environment, Network, RecordSource, Store} = require('relay-runtime');
+
+const store = new Store( new RecordSource() );
+
+const network = Network.create((operation, variables) => {
+    return fetch('http://localhost:5000/graphql', {
+        method: 'POST',
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            query: operation.text,
+            variables
+        })
+    }).then(response => {
+        return response.json()
+    }).catch(err => {
+        return err;
+    })
+});
+
+const environment = new Environment({
+    network,
+    store
+});
+
+export default environment;
